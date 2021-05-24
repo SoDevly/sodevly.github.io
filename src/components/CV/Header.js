ac730c8311cc9c2b8372ef1d926b3a353230c0ae
limsohee <zdlath@gmail.com>
2021-05-24 14:30:26 +0900

    CV 수정

--- src/components/CV/Header.js
+++ src/components/CV/Header.js
@@ -3,10 +3,13 @@ import { Row, Col } from 'antd'
 
 const Header = ({ header }) => (
   <Row type="flex" justify="space-around" align="middle">
+    
     <Col span={2} className="hide-mobile">
-      <a href="/cv.pdf" target="_blank" className="no-print">
-        <img src="/logos/pdf.svg" height="50" alt="pdf icon" />
-      </a>
+      {header.cv && (
+        <a href={header.cv.file} target="_blank" className="no-print">
+          <img src={header.cv.cvIcon} height="50" alt="pdf icon" />
+        </a>
+      )}
     </Col>
     <Col span={8} className="text-right">
       <h2 className="text-2xl">{header.name}</h2>
